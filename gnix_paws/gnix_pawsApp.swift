fe235f924f8a807a7b751fcb8f5275217de2aad0
davigomesflorencio <davigomesflorencio@gmail.com>
2026-01-29 13:43:53 -0300

    Initial Commit

---
+++ gnix_paws/gnix_pawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  gnix_pawsApp.swift
+//  gnix_paws
+//
+//  Created by Davi Gomes Florencio on 29/01/26.
+//
+
+import SwiftUI
+
+@main
+struct gnix_pawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
